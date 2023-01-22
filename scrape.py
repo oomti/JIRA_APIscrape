21f9d89724356297dbce0665ae254721ddfe8cc4
unknown <oomtig@gmail.com>
2023-01-22 16:47:00 +0000

    Update scrape.py to handle some edge cases in how the api is presented. One remaining edge case is tables

--- scrape.py
+++ scrape.py
@@ -13,15 +13,6 @@ soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find_all("div", class_="sc-ertOQY cTYMbp")
 restAPI = {}
 
-def extractParamSection(section):
-    isrequired = section.findnext(text="Required")
-    section = {
-        "name" : section.text,
-        "required" : isrequired,
-        "type" : section.findNext("p").text
-    }
-    return section
-
 for div in results:
     ahref = div.find("a")
     title = ahref.text
@@ -31,10 +22,11 @@ for div in results:
     subsoup= BeautifulSoup(subpage.content, "html.parser")
     sections = subsoup.find_all("section", class_="sc-cyQzhP tIaJL")
     for section in sections:
+
         subtitle = section.find(
             "h3", class_="sc-izUgoq jrgPRL")
         route = section.find (
-            "p", class_="sc-ibnDSj uiuRO"
+            "p"
         )
         params = {}
 
@@ -42,27 +34,29 @@ for div in results:
             subsection = section.find (
                 text=paramType
             )
-            try:
-                paramNames = subsection.parent.findNext().find_all(
-                    "strong")
-                params[paramType] = list(
-                    map(lambda param: {
-                            "name" : (param.text),
-                            "required" : param.find_next(text="Required"), 
-                            "type" : param.findNext("p").text or ""}, 
-                        paramNames))
-            except: 
-                False
+            if subsection:
+                params[paramType] = []
+                subsection = subsection.parent
+                while (subsection.next_sibling and subsection.next_sibling.find("strong")):
+                    subsection = subsection.next_sibling
+                    paramNames = subsection.find_all("strong")
+                    params[paramType] += list(
+                        map(lambda param: {
+                            "name": (param.text),
+                            "required": param.find_next(text="Required"),
+                            "type": param.findNext("p").text or ""},
+                            paramNames))
+            
+            
         if "Query parameters" in params:
             queryParams = "&".join(list(map(lambda param: 
                 "{}=".format(param['name']) + "${{param.{}}}".format(param['name']), params["Query parameters"]))).replace(" ","")
         else:
-            url = route.text.split()[1]
-            method = route.text.split()[0]
-
+            False
+        url = route.text.split()[1]
+        method = route.text.split()[0]
         name = subtitle.text.title().replace(" ","")
         name = ''.join([name[0].lower(), name[1:]])
-        pprint(name)
         restAPI[name] = {
             "name" : name,
             "method" : method,
@@ -70,7 +64,6 @@ for div in results:
             "params" : params
         }
 
-pprint(restAPI)
 
 with open("API.json","w") as outfile:
     json.dump(restAPI,outfile)
